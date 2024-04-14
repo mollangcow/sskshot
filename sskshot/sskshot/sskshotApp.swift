17b19026f9048b46d2ae23af464767b4e001c47d
SangwooKim(Moca) <mollangcow@gmail.com>
2024-04-15 01:14:39 +0900

    create: project

---
+++ sskshot/sskshot/sskshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  sskshotApp.swift
+//  sskshot
+//
+//  Created by kimsangwoo on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct sskshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
